4f291e47aa921e8412aa647797bd8bf3429eba4b
Morten Brudvik <morten@brudvik.com>
2018-03-31 22:58:49 +0200

    added pages impl. page routing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+import {RouterConfiguration, Router} from 'aurelia-router';
+import {PLATFORM} from 'aurelia-pal';
 
 export class App {
+  router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router): void {
+    this.router = router; 
+    config.title = 'Morten Brudvik';
+    config.map([
+      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('./home'), nav: true, title: 'Home' },
+      { route: ['hobbies'], name: 'hobbies', moduleId: PLATFORM.moduleName('./hobbies'), nav: true, title: 'Hobbies' },
+      { route: ['contact'], name: 'contact', moduleId: PLATFORM.moduleName('./contact'), nav: true, title: 'Contact'}
+    ]);
+  }
 }
---
+++ src/hobbies.ts
@@ -0,0 +1,1 @@
+export class Hobbies {ß}
